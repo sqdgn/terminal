27beed9ed1b1c5750292d17f24c8688b14bc4371
yohann <yohann_88@hotmail.fr>
2024-12-18 11:12:09 +0200

    improvments for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
   server: {
+    host: true,
+    port: parseInt(process.env.PORT || '3000')
+  },
+  preview: {
+    host: true,
     port: parseInt(process.env.PORT || '3000')
   }
 })
